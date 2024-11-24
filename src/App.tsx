5eef7a23cd522c048e7feee8ecadbc96a9ed720a
Pukar Timalsina <164367991+PukarCodesForLiving@users.noreply.github.com>
2024-11-24 13:54:29 -0500

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Dashboard from './components/Dashboard';
+import CitationManager from './components/CitationManager';
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
       <Navbar />
       <Dashboard />
+      <CitationManager />
     </div>
   );
 }
